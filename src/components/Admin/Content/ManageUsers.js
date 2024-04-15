614bcc650e9a79710951344cdf40074e977022ec
hatran216004 <qiqisokiu.321@gmail.com>
2024-04-16 02:47:49 +0700

    custom btn for modal add new user

--- src/components/Admin/Content/ManageUsers.js
+++ src/components/Admin/Content/ManageUsers.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import ModalCreateUser from "./ModalCreateUser";
 import "./ManageUsers.scss";
 
 const ManageUsers = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+
     return (
         <div className="manage-users-container">
-            <h1 className="manage-users-title">Title</h1>
+            <h1 className="manage-users-title">Manage Users</h1>
             <div className="users-content">
-                <button>Add new user</button>
-                <div>table users</div>
-                <ModalCreateUser />
+                <button
+                    className="btn-custom btn-add-user"
+                    onClick={() => setShow(true)}
+                >
+                    Add new user
+                </button>
+                <div className="table-user-wrapper">table users</div>
+                <ModalCreateUser show={show} handleClose={handleClose} />
             </div>
         </div>
     );
--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -3,12 +3,7 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FaCloudUploadAlt } from "react-icons/fa";
 
-const ModalCreateUser = () => {
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
+const ModalCreateUser = ({ show, handleClose }) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [username, setUsername] = useState("");
@@ -16,14 +11,14 @@ const ModalCreateUser = () => {
     const [avatar, setAvatar] = useState();
     const [image, setImage] = useState("");
 
-    // cleanup file image
+    // Cleanup file image
     useEffect(() => {
         return () => {
             avatar && URL.revokeObjectURL(avatar.preview);
         };
     }, [avatar]);
 
-    // handle upload image
+    // Handle upload image
     const handleUploadImage = (e) => {
         const file = e.target.files[0];
         file.preview = URL.createObjectURL(file);
@@ -32,111 +27,103 @@ const ModalCreateUser = () => {
     };
 
     return (
-        <>
-            <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
-            </Button>
-
-            <Modal
-                show={show}
-                onHide={handleClose}
-                centered
-                backdrop="static"
-                className="modal-add-user"
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>Add new user</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <form className="row g-3">
-                        <div className="col-md-12">
-                            <label htmlFor="inputEmail4" className="form-label">
-                                Email
-                            </label>
-                            <input
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                type="email"
-                                className="form-control"
-                                id="inputEmail4"
-                            />
-                        </div>
-                        <div className="col-md-12">
-                            <label
-                                htmlFor="inputPassword4"
-                                className="form-label"
-                            >
-                                Password
-                            </label>
-                            <input
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                type="password"
-                                className="form-control"
-                                id="inputPassword4"
-                            />
-                        </div>
-                        <div className="col-md-6">
-                            <label htmlFor="inputCity" className="form-label">
-                                Username
-                            </label>
-                            <input
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                type="text"
-                                className="form-control"
-                                id="inputCity"
-                            />
-                        </div>
-                        <div className="col-md-6">
-                            <label htmlFor="inputState" className="form-label">
-                                Role
-                            </label>
-                            <select
-                                id="inputState"
-                                className="form-select"
-                                onChange={(e) => setRole(e.target.value)}
-                            >
-                                <option defaultValue value={role}>
-                                    USER
-                                </option>
-                                <option value="ADMIN">ADMIN</option>
-                            </select>
-                        </div>
-                        <div className="col-md-12">
-                            <label
-                                className="form-label modal-btn-upload"
-                                htmlFor="upload"
-                            >
-                                <FaCloudUploadAlt className="modal-upload-icon" />
-                                Upload image
-                            </label>
-                            <input
-                                type="file"
-                                hidden
-                                id="upload"
-                                onChange={(e) => handleUploadImage(e)}
-                            />
-                        </div>
-                        <div className="img-preview">
-                            {avatar ? (
-                                <img src={avatar.preview} alt="" />
-                            ) : (
-                                <span>Preview image</span>
-                            )}
-                        </div>
-                    </form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
+        <Modal
+            show={show}
+            onHide={handleClose}
+            centered
+            backdrop="static"
+            className="modal-add-user"
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>Add new user</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <form className="row g-3">
+                    <div className="col-md-12">
+                        <label htmlFor="inputEmail4" className="form-label">
+                            Email
+                        </label>
+                        <input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="email"
+                            className="form-control"
+                            id="inputEmail4"
+                        />
+                    </div>
+                    <div className="col-md-12">
+                        <label htmlFor="inputPassword4" className="form-label">
+                            Password
+                        </label>
+                        <input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            type="password"
+                            className="form-control"
+                            id="inputPassword4"
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <label htmlFor="inputCity" className="form-label">
+                            Username
+                        </label>
+                        <input
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            type="text"
+                            className="form-control"
+                            id="inputCity"
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <label htmlFor="inputState" className="form-label">
+                            Role
+                        </label>
+                        <select
+                            value={role}
+                            id="inputState"
+                            className="form-select"
+                            onChange={(e) => setRole(e.target.value)}
+                        >
+                            <option defaultValue value="USER">
+                                USER
+                            </option>
+                            <option value="ADMIN">ADMIN</option>
+                        </select>
+                    </div>
+                    <div className="col-md-12">
+                        <label
+                            className="form-label modal-btn-upload"
+                            htmlFor="upload"
+                        >
+                            <FaCloudUploadAlt className="modal-upload-icon" />
+                            Upload image
+                        </label>
+                        <input
+                            type="file"
+                            hidden
+                            id="upload"
+                            onChange={(e) => handleUploadImage(e)}
+                        />
+                    </div>
+                    <div className="img-preview">
+                        {avatar ? (
+                            <img src={avatar.preview} alt="" />
+                        ) : (
+                            <span>Preview avatar</span>
+                        )}
+                    </div>
+                </form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Save
+                </Button>
+            </Modal.Footer>
+        </Modal>
     );
 };
 
